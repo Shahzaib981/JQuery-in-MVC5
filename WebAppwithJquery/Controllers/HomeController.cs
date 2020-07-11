ef22b944b8b68bc483fdd20ef49d3fca5d0721d1
Zaibi <zaibi@gmail.com>
2020-07-11 13:32:59 +0500

    JQuery In MVC5

---
+++ WebAppwithJquery/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppwithJquery.Models;
+using Newtonsoft.Json;
+
+namespace WebAppwithJquery.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetStudent()
+        {
+            Student std = new Student()
+            {
+                id = 1,
+                name = "Ali",
+                email = "ms@gmail.com"
+            };
+
+            var json = JsonConvert.SerializeObject(std);
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult AddStudent(Student student)
+        {
+            return Json("true", JsonRequestBehavior.AllowGet);
+        }
+    
+    
+    
+    }
+}
